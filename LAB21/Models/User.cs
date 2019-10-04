07dd0860b031418f5f6512b54f7e02feee3da36d
Flaka Mahmuti <flakamahmuti@gmail.com>
2019-10-03 21:15:55 -0400

    First commit

---
+++ LAB21/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB21.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
+        public string FirstName { get; set; }
+        [Key]
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MinLength(9)]
+        [MaxLength(9)]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Email { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(10)]
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Range(18, 99)]
+        public int Age { get; set; }
+    }
+}
